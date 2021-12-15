0eafdca4081354498a129a4c8957f7c1e0427269
Sanketh Moudgalya <sanketh.moudgalya@elucidbio.com>
2021-12-15 16:04:34 -0500

    Dec15 updates

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 
 at::Tensor inferOneImage(std::string imageFullPath)
 {
-  //Read images using opencv========================================================================
+  // Read images using opencv========================================================================
   cv::Mat cvimg = cv::imread(imageFullPath);
   cv::Mat resizedImg;
 
@@ -17,16 +17,18 @@ at::Tensor inferOneImage(std::string imageFullPath)
     std::cout << "Could not open or find the image: " << imageFullPath << std::endl;
   }
 
-  cv::resize(cvimg, resizedImg, cv::Size(75, 100));
+  cv::cvtColor(cvimg, cvimg, CV_BGR2RGB);
+  cv::resize(cvimg, resizedImg, cv::Size(100, 75), 0, 0, CV_INTER_AREA);
+  // cv::cvtColor(resizedImg, resizedImg, CV_RGB2BGR);
+  // cv::imwrite("/home/sm/Documents/work/data/hrpTestData/cppCVImg.png", resizedImg);
 
-  cv::cvtColor(resizedImg, resizedImg, CV_BGR2RGB);
   resizedImg.convertTo(resizedImg, CV_32FC3, 1.0f / 255.0f);
   auto input_tensor = torch::from_blob(resizedImg.data, {resizedImg.rows, resizedImg.cols, 3});
-  input_tensor = input_tensor.permute({2, 1, 0});
+  input_tensor = input_tensor.permute({2, 0, 1});
   input_tensor.unsqueeze_(0);
 
   // Deserialize the ScriptModule from a file using torch::jit::load().
-  std::string filename = "/home/sm/Documents/work/dl/cpp-pytorch/libtorch_Modified_banded_w_grand_best_achieved_acc.pt";
+  std::string filename = "/home/sm/Documents/work/dl/cpp-pytorch/libtorch_Modified_banded_w_grand_best_achieved_acc_old.pt";
   torch::jit::script::Module module = torch::jit::load(filename);
   module.eval();
 
@@ -35,16 +37,18 @@ at::Tensor inferOneImage(std::string imageFullPath)
   inputs.push_back(input_tensor.to(at::kCPU));
 
   // // Execute the model and turn its output into a tensor.
-  at::Tensor output = module.forward(inputs).toTensor();
+  at::Tensor modelOut = module.forward(inputs).toTensor();
+  at::Tensor finalOut = torch::softmax(modelOut, 1);
 
-  return output;
+  return finalOut;
 }
 
 int main()
 {
-  //Get image paths================================================================================
-  // std::string dataPath = "/home/sm/Documents/work/data/inferData/CommonCarotidArtery/unwrapped/";
+  // Get image paths================================================================================
+  //  std::string dataPath = "/home/sm/Documents/work/data/inferData/CommonCarotidArtery/unwrapped/";
   std::string dataPath = "/home/sm/Documents/work/data/hrpTestData/";
+  std::string dataOutPath = "/home/sm/Documents/work/dl/cpp-pytorch/";
   std::string ext = ".png";
   std::vector<std::string> unwrappedImages;
 
@@ -52,14 +56,14 @@ int main()
   {
     // std::string imgPath = img.path().filename().u8string();
     std::string imgPath = img.path().u8string();
-    //std::string fullPath = dataPath + imgPath;
+    // std::string fullPath = dataPath + imgPath;
 
     if ((imgPath.find(ext) != std::string::npos) &&
         (imgPath.find("ct") == std::string::npos) &&
         (imgPath.find("Histology") == std::string::npos) &&
         (imgPath.find("donut") == std::string::npos))
     {
-      //unwrappedImages.push_back(dataPath + imgPath);
+      // unwrappedImages.push_back(dataPath + imgPath);
       unwrappedImages.push_back(imgPath);
     }
   }
@@ -67,21 +71,21 @@ int main()
 
   // for (auto i : unwrappedImages)
   //   std::cout << i << std::endl;
-  //std::cout << unwrappedImages.at(0) << std::endl;
+  // std::cout << unwrappedImages.at(0) << std::endl;
   // File to write out to
   std::ofstream csvFile;
-  csvFile.open(dataPath + "out.csv");
+  csvFile.open(dataOutPath + "outCppCV.csv");
 
   for (auto i : unwrappedImages)
   {
-    std::cout << "Inferring unwrapped image: " << i << std::endl;
+    // std::cout << "Inferring unwrapped image: " << i << std::endl;
     at::Tensor output = inferOneImage(i);
     cv::Mat outArr(3, 1, CV_32F, output.data_ptr<float>());
     std::string outVal0(std::to_string(outArr.at<float>(0)));
     std::string outVal1(std::to_string(outArr.at<float>(1)));
     std::string outVal2(std::to_string(outArr.at<float>(2)));
 
-    csvFile << i + "," + outVal0 + "," + outVal1 + "," + outVal2 + ",\n";
+    csvFile << i + "," + outVal0 + "," + outVal1 + "," + outVal2 + "\n";
   }
   csvFile.close();
   return 0;
